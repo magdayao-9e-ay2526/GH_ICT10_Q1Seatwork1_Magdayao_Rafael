71c0df8434504420d564bd979680126ebaab9677
Magdayao-9E-ay2526 <magdayao.ra@obmontessori.edu.ph>
2026-08-13 11:34:48 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+from pyscript import display
+
+your_name = 'Rafael Sancho A. Magdayao'
+age = 16
+height = 163.3
+
+countries = ['Switzerland', 'Norway', 'Canada']
+
+student_type = False
+
+me_dict = {
+    'color': 'Blue',
+    'car_brand': 'Bentley',
+    'shoe_size': 9,
+    'best_friend': 'Athena Alfaro'
+}
+
+favorite_fruits = {
+    'grapes',
+    'apples',
+    'banana',
+    'cherry',
+    'blackberry'
+}
+
+days_of_week = (
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+)
+
+display(f'Hello! My name is {your_name}.')
+display(f'I am {age} years old.')
+display(f'My height is {height} cm.')
+display(f'The three countries I want to visit are {countries}.')
+display(f'New student: {student_type}.')
+display(f'The things to know about me: {me_dict}.')
+display(f'My 5 favorite fruits are {favorite_fruits}.')
+display(f'The days of the week are {days_of_week}.')
